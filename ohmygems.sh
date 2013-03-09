701408464de6047481188126e6fe1c3bc45b251d
Jeremy Hinegardner <jeremy@hinegardner.org>
2013-03-09 16:22:07 +0100

    prefixing variables

--- ohmygems.sh
+++ ohmygems.sh
@@ -1,47 +1,66 @@
-export ORIG_GEM_PATH=${GEM_PATH:-}
-export ORIG_GEM_HOME=${GEM_HOME:-}
-export ORIG_PATH=${PATH}
+# Variables to store the current state of affairs
+export OMG_ORIG_GEM_PATH=${GEM_PATH:-}
+export OMG_ORIG_GEM_HOME=${GEM_HOME:-}
+export OMG_ORIG_PATH=${PATH:-}
+
+# items we use to set everything up
+export OMG_RUBY=$(ruby_version)
+export OMG_GEMSET=
+export OMG_REPO_ROOT=${OMG_REPO_ROOT:-${HOME}/.gem/repos}
 
 ohmygems() {
-    name=${1:-}
+    local name=${1:-}
+    export OMG_RUBY=$(ruby_version)
+
+    local rv_repo=${OMG_REPO_ROOT}/${OMG_RUBY}
+    local gem_dir=
+
 
     if [ -z "$name" ]; then
         echo "usage: ohmygems <name or reset>"
         echo
         echo "  Switches gem home to a (potentially new) named repo."
         echo "  Your previous gems are still visible,"
-        echo "  but new gem installs will install into ~/.gems/repos/<name>."
+        echo "  but new gem installs will install into ${rv_repo}/<name>."
         echo "  Use 'reset' to go back to normal."
         echo
-        echo "Available repos:"
+        echo "Available repos for ${OMG_RUBY}:"
         echo
-        ls ~/.gem/repos | pr -o2 -l1 | sed "s/^  ${OHMYGEMS}$/* ${OHMYGEMS}/g"
+        ls ${rv_repo} | pr -o2 -l1 | sed "s/^  ${OMG_GEMSET}$/* ${OMG_GEMSET}/g"
         echo
         return
     elif [ $name = "reset" ]; then
         echo Resetting repo
 
-        if [ -z "$ORIG_GEM_HOME" ]; then
+        if [ -z "${OMG_ORIG_GEM_HOME}" ]; then
             unset GEM_HOME
         else
-            GEM_HOME=${ORIG_GEM_HOME}
+            export GEM_HOME=${OMG_ORIG_GEM_HOME}
         fi
 
-        GEM_PATH=${ORIG_GEM_PATH}
-        PATH=$ORIG_PATH
-        unset OHMYGEMS
-    else
-        echo Switching to $name
+        if [ -z "${OMG_ORIG_GEM_PATH}" ]; then
+          unset GEM_PATH
+        else
+          export GEM_PATH=${OMG_ORIG_GEM_PATH}
+        fi
 
-        export GEM_HOME=~/.gem/repos/${name}
-        export GEM_PATH=${ORIG_GEM_HOME}:${ORIG_GEM_PATH}
-        export PATH=${ORIG_PATH}:${GEM_HOME}/bin
-        export OHMYGEMS=$name
+        export PATH=${OMG_ORIG_PATH}
+        unset OMG_GEMSET
+        export OMG_GEMSET=
+    else
+        echo Switching to ${name}
+        gem_dir=${rv_repo}/${name}
+
+        export GEM_HOME=${gem_dir}
+        export GEM_PATH=${gem_dir}:${GEM_PATH}
+        export PATH=${gem_dir}/bin:${PATH}
+        export OMG_GEMSET=${name}
     fi
 
     echo
     echo GEM_PATH=${GEM_PATH:-not set}
     echo GEM_HOME=${GEM_HOME:-not set}
     echo PATH=$PATH
-    echo OHMYGEMS=${OHMYGEMS:-not set}
 }
+
+alias omg=ohmygems
